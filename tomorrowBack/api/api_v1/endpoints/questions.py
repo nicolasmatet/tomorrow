a9c12091c6d538ebd3e018c6a233bb78684125c4
nicolas.matet <nicolas.matet@algonomia.com>
2022-04-14 17:27:33 +0200

    add images questions randomize question

--- tomorrowBack/api/api_v1/endpoints/questions.py
+++ tomorrowBack/api/api_v1/endpoints/questions.py
@@ -1,14 +1,21 @@
-import os
-
-import boto3
-from botocore.exceptions import ClientError
 from fastapi import APIRouter
+from ..dynamodb.fixtures import FixtureQuestions
 from ..models import Question
-
+import datetime
 router = APIRouter()
 
+NQUESTIONS = len(FixtureQuestions().values)
+SEED = 59863
+def get_day_in_year():
+    return datetime.datetime.now().timetuple().tm_yday
+
+def get_question_id(question_order:int):
+    initial = get_day_in_year()
+    return (SEED * (initial + question_order)) % NQUESTIONS
 
 @router.get("/")
 async def get_question(n):
-    return Question.read({'id': int(n)+26})
+    print(n)
+    question_id = get_question_id(int(n))
+    return Question.read({'id': question_id})
 
