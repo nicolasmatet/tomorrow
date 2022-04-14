41d8b063cf703620ab25f89c28df52fa4e7f8b19
nicolas.matet <nicolas.matet@algonomia.com>
2022-04-14 17:44:01 +0200

    add all questions randomize question

--- tomorrowBack/api/api_v1/endpoints/questions.py
+++ tomorrowBack/api/api_v1/endpoints/questions.py
@@ -11,11 +11,10 @@ def get_day_in_year():
 
 def get_question_id(question_order:int):
     initial = get_day_in_year()
-    return (SEED * (initial + question_order)) % NQUESTIONS
+    return (initial + SEED ** question_order) % NQUESTIONS
 
 @router.get("/")
 async def get_question(n):
-    print(n)
     question_id = get_question_id(int(n))
     return Question.read({'id': question_id})
 
